e7215b2925c726474c776e716f339b3a7ba99d16
Hugo <hugo.hornquist@gmail.com>
2015-08-03 14:34:15 +0200

    minor changes

--- asciiChart.cpp
+++ asciiChart.cpp
@@ -3,19 +3,58 @@
 #include <stdio.h>
 using namespace std;
 
-int main()
-{
+int main(int argc, char* argv[]) {
 
 char currentChar;
+bool isHex = true;
 
-for(int i = 0; i < 256; i++)
-{
-currentChar = i;
+if(argc >= 2) {
+	switch (argv[1]) {
+		case "--help": {
+			
+			break;
+		}
+		case "dec": {
+			isHex = false;
+			break;
+		}
+		case "hex": {
+			isHex = true;	
+			break;
+		}
 
-cout<<i<<". "<<currentChar<<"    ";
+		
+	}
 
-if(i % 6 == 0)
-	cout<<endl;
+}
+
+
+
+for(int i = 0; i < 256; i++) {
+	currentChar = i;
+
+	if(isHex) {
+		cout << hex << i << ". " << currentChar << "    ";
+	} else {
+		cout << dec << i << ". " << currentChar << "    ";
+	}
+
+	if(i % 6 == 0) {
+		cout << endl;
+	}
+}
+
+cout << endl << "Press enter to continue...";
+getchar();
+
+return EXIT_SUCCESS;
+}
+		cout<<i<<". "<<currentChar<<"    ";
+	}
+
+	if(i % 6 == 0) {
+		cout<<endl;
+	}
 }
 
 cout << endl << "Press enter to continue...";
